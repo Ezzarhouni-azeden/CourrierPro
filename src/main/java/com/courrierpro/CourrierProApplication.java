9227bf254da39267c31bb2ab4ae4ac762d921fe1
Azd <azedenezzair@gmail.com>
2025-03-02 00:51:57 +0000

    Premier commit - Initialisation du projet

---
+++ src/main/java/com/courrierpro/CourrierProApplication.java
@@ -0,0 +1,13 @@
+package com.courrierpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourrierProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourrierProApplication.class, args);
+    }
+
+}
